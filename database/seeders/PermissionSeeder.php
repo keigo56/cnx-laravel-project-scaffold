6154f1094ade123ef2ac632abb506f77923a0ad2
Keigo Fujita <keigovfujita@gmail.com>
2023-07-03 03:15:31 +0800

    User Role Management
    - Installed required packages
    - Added trait to User Model
    - Create Role and permission seeders

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name' => 'view_users']);
+        Permission::create(['name' => 'add_users']);
+        Permission::create(['name' => 'delete_users']);
+        Permission::create(['name' => 'edit_users']);
+        Permission::create(['name' => 'import_users']);
+        Permission::create(['name' => 'export_users']);
+        Permission::create(['name' => 'view_roles']);
+        Permission::create(['name' => 'add_roles']);
+        Permission::create(['name' => 'delete_roles']);
+        Permission::create(['name' => 'edit_roles']);
+        Permission::create(['name' => 'import_roles']);
+        Permission::create(['name' => 'export_roles']);
+        Permission::create(['name' => 'view_permissions']);
+    }
+}
