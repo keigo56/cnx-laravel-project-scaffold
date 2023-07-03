b0497cffe355ee9f75d4da3d99ad7fc39d3a2191
Keigo Fujita <keigovfujita@gmail.com>
2023-07-03 15:14:22 +0800

    Added Activity Log Functionality

---
+++ app/Http/Controllers/API/AuditTrailController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Modules\Datatables\Column;
+use App\Modules\Datatables\DataTable;
+use App\Modules\Datatables\Row;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Spatie\Activitylog\Models\Activity;
+use Spatie\Permission\Models\Permission;
+
+class AuditTrailController extends Controller
+{
+    private DataTable $datatable;
+
+    public function __construct()
+    {
+        $this->datatable =  DataTable::init()
+            ->query(
+                DB::table(Activity::query()
+                    ->select(
+                        DB::raw('activity_log.id as activity_log_id'),
+                        'activity_log.description',
+                        'activity_log.causer_id',
+                        DB::raw('activity_log.created_at as activity_log_created_at'),
+                        'users.email'
+                    )
+                    ->join('users', 'users.id', '=', 'activity_log.causer_id'))
+            )
+            ->columns([
+                Column::make()
+                    ->key('activity_log_id')
+                    ->label('ID')
+                    ->numeric()
+                    ->visible(false)
+                ,
+                Column::make()
+                    ->key('email')
+                    ->label('User Email')
+                    ->string()
+                ,
+                Column::make()
+                    ->key('description')
+                    ->label('Activity')
+                    ->string()
+                ,
+                Column::make()
+                    ->key('activity_log_created_at')
+                    ->label('Performed At')
+                    ->date()
+                ,
+            ])
+        ;
+    }
+
+    public function dataset(Request $request)
+    {
+        try {
+            $datatable =
+                $this->datatable
+                    ->filter($request->input('filters'))
+                    ->search($request->input('search'))
+                    ->mapRow(function(Row $row){
+                        $row->setValue(
+                            'activity_log_created_at',
+                            Carbon::parse($row->getValue('activity_log_created_at'))
+                                ->timezone('Asia/Manila')
+                                ->format('Y-m-d h:i A')
+                        );
+                    })
+                    ->sortBy($request->input('sort_by'), $request->input('sort_direction'))
+                    ->paginate()
+            ;
+
+            return response()->json([
+                'datatable' => $datatable
+            ]);
+
+        }catch (Exception $exception){
+            return response()->json([
+                'error' => true,
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function column_distinct_values(Request $request)
+    {
+        $request->validate([
+            'column' => ['required', 'string']
+        ]);
+
+        try {
+
+            $distinct_values =
+                $this->datatable
+                    ->filter($request->input('filters'))
+                    ->search($request->input('search'))
+                    ->distinct($request->input('column'))
+            ;
+
+            return response()->json([
+                'column' => $request->input('column'),
+                'distinct_values' => $distinct_values
+            ]);
+
+        }catch (Exception $exception){
+            return response()->json([
+                'error' => true,
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function export(Request $request)
+    {
+        try {
+
+            $this->datatable
+                ->filter($request->input('filters'))
+                ->search($request->input('search'))
+                ->sortBy($request->input('sort_by'), $request->input('sort_direction'))
+                ->exportName('logs')
+                ->export();
+
+        }catch (Exception $exception){
+            return response()->json([
+                'error' => true,
+                'message' => $exception->getMessage(),
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,14 +20,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->delete('/auth/logout', [\App\Http\Controllers\Authentication\AzureSSOController::class, 'sso_logout']);
 
-Route::post('/datatable/users', [\App\Http\Controllers\Api\UserController::class, 'dataset']);
-Route::post('/datatable/users/column-distinct-values', [\App\Http\Controllers\Api\UserController::class, 'column_distinct_values']);
-Route::post('/datatable/users/export', [\App\Http\Controllers\Api\UserController::class, 'export']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/datatable/users', [\App\Http\Controllers\Api\UserController::class, 'dataset']);
+    Route::post('/datatable/users/column-distinct-values', [\App\Http\Controllers\Api\UserController::class, 'column_distinct_values']);
+    Route::post('/datatable/users/export', [\App\Http\Controllers\Api\UserController::class, 'export']);
 
-Route::post('/datatable/roles', [\App\Http\Controllers\Api\RoleController::class, 'dataset']);
-Route::post('/datatable/roles/column-distinct-values', [\App\Http\Controllers\Api\RoleController::class, 'column_distinct_values']);
-Route::post('/datatable/roles/export', [\App\Http\Controllers\Api\RoleController::class, 'export']);
+    Route::post('/datatable/roles', [\App\Http\Controllers\Api\RoleController::class, 'dataset']);
+    Route::post('/datatable/roles/column-distinct-values', [\App\Http\Controllers\Api\RoleController::class, 'column_distinct_values']);
+    Route::post('/datatable/roles/export', [\App\Http\Controllers\Api\RoleController::class, 'export']);
 
-Route::post('/datatable/permissions', [\App\Http\Controllers\Api\PermissionController::class, 'dataset']);
-Route::post('/datatable/permissions/column-distinct-values', [\App\Http\Controllers\Api\PermissionController::class, 'column_distinct_values']);
-Route::post('/datatable/permissions/export', [\App\Http\Controllers\Api\PermissionController::class, 'export']);
+    Route::post('/datatable/permissions', [\App\Http\Controllers\Api\PermissionController::class, 'dataset']);
+    Route::post('/datatable/permissions/column-distinct-values', [\App\Http\Controllers\Api\PermissionController::class, 'column_distinct_values']);
+    Route::post('/datatable/permissions/export', [\App\Http\Controllers\Api\PermissionController::class, 'export']);
+
+    Route::post('/datatable/audit-trail/logs', [\App\Http\Controllers\Api\AuditTrailController::class, 'dataset']);
+    Route::post('/datatable/audit-trail/logs/column-distinct-values', [\App\Http\Controllers\Api\AuditTrailController::class, 'column_distinct_values']);
+    Route::post('/datatable/audit-trail/logs/export', [\App\Http\Controllers\Api\AuditTrailController::class, 'export']);
+});
