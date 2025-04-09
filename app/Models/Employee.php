cdab38fa896ac8eae6bf79d80ce6ed4f3857ce73
Keigo Victor Fujita <keigovfujita@gmail.com>
2025-04-10 01:16:29 +0800

    chore (app/Models): align project files with Laravel 12 structure and conventions

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
     protected $primaryKey = 'workday_id';
     protected $table = "employees";
-    use HasFactory;
 }
