c51e28c4e9a3cb9492fe4c1add3383906fc1155b
Keigo Victor Fujita <keigovfujita@gmail.com>
2025-04-10 01:33:19 +0800

    chore: align Spatie Roles and Permissions setup with Laravel 12

--- config/permission.php
+++ config/permission.php
@@ -13,19 +13,18 @@ return [
          * `Spatie\Permission\Contracts\Permission` contract.
          */
 
-        'permission' => Spatie\Permission\Models\Permission::class,
+         'permission' => Spatie\Permission\Models\Permission::class,
 
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * Eloquent model should be used to retrieve your roles. Of course, it
-         * is often just the "Role" model but you may use whatever you like.
-         *
-         * The model you want to use as a Role model needs to implement the
-         * `Spatie\Permission\Contracts\Role` contract.
-         */
-
-        'role' => Spatie\Permission\Models\Role::class,
-
+         /*
+          * When using the "HasRoles" trait from this package, we need to know which
+          * Eloquent model should be used to retrieve your roles. Of course, it
+          * is often just the "Role" model but you may use whatever you like.
+          *
+          * The model you want to use as a Role model needs to implement the
+          * `Spatie\Permission\Contracts\Role` contract.
+          */
+ 
+         'role' => Spatie\Permission\Models\Role::class,
     ],
 
     'table_names' => [
@@ -36,47 +35,47 @@ return [
          * default value but you may easily change it to any table you like.
          */
 
-        'roles' => 'roles',
+         'roles' => 'roles',
 
-        /*
-         * When using the "HasPermissions" trait from this package, we need to know which
-         * table should be used to retrieve your permissions. We have chosen a basic
-         * default value but you may easily change it to any table you like.
-         */
-
-        'permissions' => 'permissions',
-
-        /*
-         * When using the "HasPermissions" trait from this package, we need to know which
-         * table should be used to retrieve your models permissions. We have chosen a
-         * basic default value but you may easily change it to any table you like.
-         */
-
-        'model_has_permissions' => 'model_has_permissions',
-
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * table should be used to retrieve your models roles. We have chosen a
-         * basic default value but you may easily change it to any table you like.
-         */
-
-        'model_has_roles' => 'model_has_roles',
-
-        /*
-         * When using the "HasRoles" trait from this package, we need to know which
-         * table should be used to retrieve your roles permissions. We have chosen a
-         * basic default value but you may easily change it to any table you like.
-         */
-
-        'role_has_permissions' => 'role_has_permissions',
+         /*
+          * When using the "HasPermissions" trait from this package, we need to know which
+          * table should be used to retrieve your permissions. We have chosen a basic
+          * default value but you may easily change it to any table you like.
+          */
+ 
+         'permissions' => 'permissions',
+ 
+         /*
+          * When using the "HasPermissions" trait from this package, we need to know which
+          * table should be used to retrieve your models permissions. We have chosen a
+          * basic default value but you may easily change it to any table you like.
+          */
+ 
+         'model_has_permissions' => 'model_has_permissions',
+ 
+         /*
+          * When using the "HasRoles" trait from this package, we need to know which
+          * table should be used to retrieve your models roles. We have chosen a
+          * basic default value but you may easily change it to any table you like.
+          */
+ 
+         'model_has_roles' => 'model_has_roles',
+ 
+         /*
+          * When using the "HasRoles" trait from this package, we need to know which
+          * table should be used to retrieve your roles permissions. We have chosen a
+          * basic default value but you may easily change it to any table you like.
+          */
+ 
+         'role_has_permissions' => 'role_has_permissions',
     ],
 
     'column_names' => [
         /*
          * Change this if you want to name the related pivots other than defaults
          */
-        'role_pivot_key' => null, //default 'role_id',
-        'permission_pivot_key' => null, //default 'permission_id',
+        'role_pivot_key' => null, // default 'role_id',
+        'permission_pivot_key' => null, // default 'permission_id',
 
         /*
          * Change this if you want to name the related model primary key other than
@@ -104,36 +103,77 @@ return [
     'register_permission_check_method' => true,
 
     /*
-     * When set to true the package implements teams using the 'team_foreign_key'. If you want
-     * the migrations to register the 'team_foreign_key', you must set this to true
-     * before doing the migration. If you already did the migration then you must make a new
-     * migration to also add 'team_foreign_key' to 'roles', 'model_has_roles', and
-     * 'model_has_permissions'(view the latest version of package's migration file)
+     * When set to true, Laravel\Octane\Events\OperationTerminated event listener will be registered
+     * this will refresh permissions on every TickTerminated, TaskTerminated and RequestTerminated
+     * NOTE: This should not be needed in most cases, but an Octane/Vapor combination benefited from it.
      */
-
-    'teams' => false,
+    'register_octane_reset_listener' => false,
 
     /*
-     * When set to true, the required permission names are added to the exception
-     * message. This could be considered an information leak in some contexts, so
-     * the default setting is false here for optimum safety.
+     * Events will fire when a role or permission is assigned/unassigned:
+     * \Spatie\Permission\Events\RoleAttached
+     * \Spatie\Permission\Events\RoleDetached
+     * \Spatie\Permission\Events\PermissionAttached
+     * \Spatie\Permission\Events\PermissionDetached
+     *
+     * To enable, set to true, and then create listeners to watch these events.
      */
-
-    'display_permission_in_exception' => false,
+    'events_enabled' => false,
 
     /*
-     * When set to true, the required role names are added to the exception
-     * message. This could be considered an information leak in some contexts, so
-     * the default setting is false here for optimum safety.
+     * Teams Feature.
+     * When set to true the package implements teams using the 'team_foreign_key'.
+     * If you want the migrations to register the 'team_foreign_key', you must
+     * set this to true before doing the migration.
+     * If you already did the migration then you must make a new migration to also
+     * add 'team_foreign_key' to 'roles', 'model_has_roles', and 'model_has_permissions'
+     * (view the latest version of this package's migration file)
      */
 
-    'display_role_in_exception' => false,
+     'teams' => false,
+
+    /*
+     * The class to use to resolve the permissions team id
+     */
+    'team_resolver' => \Spatie\Permission\DefaultTeamResolver::class,
+
+    /*
+     * Passport Client Credentials Grant
+     * When set to true the package will use Passports Client to check permissions
+     */
+
+     'use_passport_client_credentials' => false,
+
+    /*
+     * When set to true, the required permission names are added to exception messages.
+     * This could be considered an information leak in some contexts, so the default
+     * setting is false here for optimum safety.
+     */
+
+     'display_permission_in_exception' => false,
+
+    /*
+     * When set to true, the required role names are added to exception messages.
+     * This could be considered an information leak in some contexts, so the default
+     * setting is false here for optimum safety.
+     */
+
+     'display_role_in_exception' => false,
 
     /*
      * By default wildcard permission lookups are disabled.
+     * See documentation to understand supported syntax.
      */
 
-    'enable_wildcard_permission' => false,
+     'enable_wildcard_permission' => false,
+
+    /*
+     * The class to use for interpreting wildcard permissions.
+     * If you need to modify delimiters, override the class and specify its name here.
+     */
+    // 'permission.wildcard_permission' => Spatie\Permission\WildcardPermission::class,
+
+    /* Cache-specific settings */
 
     'cache' => [
 
@@ -156,6 +196,6 @@ return [
          * file. Using 'default' here means to use the `default` set in cache.php.
          */
 
-        'store' => 'array',
+        'store' => 'array', // Change to array to disable caching
     ],
 ];
