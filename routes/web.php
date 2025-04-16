af9b902c1733092b574677aa288ee704620ea259
Keigo Victor Fujita <keigovfujita@gmail.com>
2025-04-16 18:51:01 +0800

    refactor(auth): replace BasicAuthHandler with UserBasicAuthController in web routes
    Replaced all references to BasicAuthHandler with UserBasicAuthController
    in `routes/web.php`

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Auth\Handlers\BasicAuthHandler;
 use App\Http\Controllers\Authentication\AdminAuthSSOController;
+use App\Http\Controllers\Authentication\UserBasicAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,16 +23,16 @@ Route::middleware('guest')
         Route::get('sso/callback', [AdminAuthSSOController::class, 'callback'])->name('admin.auth.sso.callback');
 
         // BASIC AUTHENTICATION
-        Route::post('user/auth/login', [BasicAuthHandler::class, 'login'])->name('user.auth.login');
-        Route::post('user/auth/login/otp/refresh', [BasicAuthHandler::class, 'refreshOtp'])->name('user.auth.login.otp.refresh');
-        Route::post('user/auth/login/otp/verify', [BasicAuthHandler::class, 'verifyOtp'])->name('user.auth.login.otp.verify');
+        Route::post('user/auth/login', [UserBasicAuthController::class, 'login'])->name('user.auth.login');
+        Route::post('user/auth/login/otp/refresh', [UserBasicAuthController::class, 'refreshOtp'])->name('user.auth.login.otp.refresh');
+        Route::post('user/auth/login/otp/verify', [UserBasicAuthController::class, 'verifyOtp'])->name('user.auth.login.otp.verify');
     });
 
 Route::middleware('auth:sanctum')
     ->group(function () {
         Route::post('admin/auth/token/validate', [AdminAuthSSOController::class, 'validateToken'])->name('admin.auth.token.validate');
-        Route::post('user/auth/token/validate', [BasicAuthHandler::class, 'validateToken'])->name('user.auth.token.validate');
+        Route::post('user/auth/token/validate', [UserBasicAuthController::class, 'validateToken'])->name('user.auth.token.validate');
 
         Route::delete('admin/auth/logout', [AdminAuthSSOController::class, 'logout'])->name('admin.auth.logout');
-        Route::delete('user/auth/logout', [BasicAuthHandler::class, 'logout'])->name('user.auth.logout');
+        Route::delete('user/auth/logout', [UserBasicAuthController::class, 'logout'])->name('user.auth.logout');
     });
